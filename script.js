a062132631f877beb7cabe1556a80228004e9080
dark1030 <poovarasanarumugam1030@gmail.com>
2024-02-07 15:38:51 +0530

    V1

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let password = document.getElementById("password");
+let password2 = document.getElementById("password2");
 let eyecon = document.querySelector(".eye-icon");
+let eyecon2 = document.querySelector(".eye-icon2");
 
 eyecon.addEventListener("click", function () {
   if (password.getAttribute("type") == "password") {
@@ -10,3 +12,40 @@ eyecon.addEventListener("click", function () {
     eyecon.setAttribute("name", "eye-outline");
   }
 });
+eyecon2.addEventListener("click", function () {
+  if (password2.getAttribute("type") == "password") {
+    password2.setAttribute("type", "text");
+    eyecon2.setAttribute("name", "eye-off-outline");
+  } else {
+    password2.setAttribute("type", "password");
+    eyecon2.setAttribute("name", "eye-outline");
+  }
+});
+
+// Card swipe
+
+let logincard = document.querySelector(".login-container");
+let signupcard = document.querySelector(".signup-container");
+
+let loginToggle = document.querySelector(".loginToggle");
+let signupToggle = document.querySelector(".signupToggle");
+
+// loginToggle.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   logincard.style.zIndex = "2";
+//   signupcard.style.zIndex = "1";
+// });
+// signupToggle.addEventListener("click", () => {
+//   signupcard.style.zIndex = "2";
+//   logincard.style.zIndex = "1";
+// });
+
+function signtoggle() {
+  logincard.style.zIndex = "1";
+  signupcard.style.zIndex = "2";
+}
+
+function logtoggle() {
+  signupcard.style.zIndex = "1";
+  logincard.style.zIndex = "2";
+}
